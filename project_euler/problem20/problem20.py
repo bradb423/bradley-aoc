6d7b5b849c35c3c4e5ed90678d7d096259e8632a
Bradley Burns <bradley.burns@codethink.co.uk>
2021-08-19 13:43:15 +0100

    euler problem 20

---
+++ project_euler/problem20/problem20.py
@@ -0,0 +1,18 @@
+# find the sum of the digits in the number 100!
+# use my answers to qs 15 and 16
+
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def fact(n):
+    if n == 1:
+        return n
+    else:
+        return n * fact(n - 1)
+
+def sumdigits(n):
+    return sum(map(int, str(n)))
+
+
+print(sumdigits(fact(10)))
---
+++ test_cases/test_euler/test_e_problem_20.py
@@ -0,0 +1,4 @@
+from project_euler.problem20.problem20 import sumdigits, fact
+
+def test_sum():
+    assert sumdigits(fact(10)) == 27
