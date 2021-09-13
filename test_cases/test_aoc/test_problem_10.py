c7dfb11761e9d681e110cf514969865e904b0fdb
Bradley Burns <bradley.burns@codethink.co.uk>
2021-09-13 16:05:09 +0100

    Add aoc problem 10
    This one was a lovely problem which I had a nice solution to, remember how the itertools groupby funciton works in future!

---
+++ test_cases/test_aoc/test_problem_10.py
@@ -0,0 +1,16 @@
+from advent_of_code.problem10.problem10 import look_and_say
+
+def test_1():
+    assert look_and_say("1",1) == "11"
+
+def test_2():
+    assert look_and_say("11",1) == "21"
+
+def test_3():
+    assert look_and_say("21",1) == "1211"
+
+def test_4():
+    assert look_and_say("1211",1) == "111221"
+
+def test_5():
+    assert look_and_say("111221",1) == "312211"
