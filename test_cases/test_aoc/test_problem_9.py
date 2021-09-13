93afbf695e37641300a1dc27f393486ad8acba87
Bradley Burns <bradley.burns@codethink.co.uk>
2021-09-13 11:03:29 +0100

    Add problem 9 of aoc along with tests
    Added my solution to problem 9, this one stumped me for a while, I kept getting an answer too short until i realised i had deleted one line of the code that would have allowed me to get the correct answer.

---
+++ test_cases/test_aoc/test_problem_9.py
@@ -0,0 +1,23 @@
+from advent_of_code.problem9.problem9 import parse_file, shortest_distance, longest_distance
+
+def test_part_1():
+    filepath = "advent_of_code/problem9/test_data.txt"
+    city_names, pair_distance_dict = parse_file(filepath)
+
+    assert shortest_distance(pair_distance_dict,city_names) == 605
+
+def test_part_2():
+    filepath = "advent_of_code/problem9/data.txt"
+    city_names, pair_distance_dict = parse_file(filepath)
+    assert shortest_distance(pair_distance_dict,city_names) == 207
+
+def test_part_3():
+    filepath = "advent_of_code/problem9/test_data.txt"
+    city_names, pair_distance_dict = parse_file(filepath)
+
+    assert longest_distance(pair_distance_dict,city_names) == 982
+
+def test_part_4():
+    filepath = "advent_of_code/problem9/data.txt"
+    city_names, pair_distance_dict = parse_file(filepath)
+    assert longest_distance(pair_distance_dict,city_names) == 804
