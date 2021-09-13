8837669895cb2c93f93f77b94443019323ada5cf
Bradley Burns <bradley.burns@codethink.co.uk>
2021-09-13 11:40:15 +0100

    Add euler problem 22
    Added problem 22 to project euler, along with the test given in the example, this was quite a simple problem and did not require much.

---
+++ project_euler/problem22/problem22.py
@@ -0,0 +1,50 @@
+import sys
+
+def get_alphabet_values():
+    alphabet_values = {}
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    length = len(alphabet)
+    for i in range(0,length):
+        character = alphabet[i]
+        alphabet_values[character] = i + 1
+    return alphabet_values
+
+
+
+def parse_file(filepath):
+    # return the parsed list of names
+    name_list = []
+    with open(filepath,"r") as file:
+        for line in file:
+            line = line.strip().split(",")
+            for name in line:
+                name = name[1:-1]
+                name_list.append(name)
+    name_list.sort()
+    return name_list
+
+def get_name_score(name, position, alphabet_values):
+    name_score = 0
+    for character in name:
+        name_score += alphabet_values[character]
+    name_score *= position
+    return name_score
+
+def sum_of_name_scores(name_list, alphabet_values):
+    score_sum = 0
+    length = len(name_list)
+    for i in range(0,length):
+        position = i+1
+        name = name_list[i]
+        score_sum += get_name_score(name, position, alphabet_values)
+    return score_sum
+
+
+def main():
+    alphabet_values = get_alphabet_values()
+    filepath = sys.argv[1]
+    name_list = parse_file(filepath)
+    print(sum_of_name_scores(name_list, alphabet_values))
+
+if __name__ == '__main__':
+    main()
